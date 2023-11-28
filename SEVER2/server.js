4a7d73c22e411c217fccc27f95c9bd6aa446202c
Reizora <152237158+Reizora1@users.noreply.github.com>
2023-11-28 14:03:56 +0800

    Add files via upload

---
+++ SEVER2/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+let payload;
+let data;
+let amountPaid;
+let transactionID;
+let transactionDate;
+let transactionSummary = "No Data received.";
+
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Middleware to ignore requests for favicon.ico
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end(); // Send a 204 No Content response
+});
+
+// Webhook endpoint to handle POST requests
+app.post('/', (req, res) => {
+  // Handle the webhook payload containing json data of events triggered from PayMongo.
+  payload = req.body;
+  // Convert payload json data into a string and assign to global variable named data.
+  data = JSON.stringify(payload, null, 2);
+
+  try {
+    // Check if the payload and its properties exist before accessing them
+    if(payload) {
+      amountPaid = JSON.stringify(payload.paid_amount, null, 2);
+      transactionID = JSON.stringify(payload.id, null, 2);
+      transactionDate = JSON.stringify(payload.paid_at, null, 2);
+
+      transactionSummary = amountPaid + " " + transactionID + " " + transactionDate;
+
+      console.log('\nId:', transactionID);
+      console.log('Amount Paid:', amountPaid);
+      console.log('Date:', transactionDate);
+    }
+    else {
+      transactionSummary = `Invalid payload structure`;
+    }
+    // Display the entire payload to the console.
+    console.log('\nReceived data:', payload);
+    console.log('Received data object:\n', data);
+    
+    // 2xx response from our server to paymongo indicating successful data acquisition.
+    res.status(200).send('Data received.');
+  }
+  catch(error) {
+    console.error('Error processing payload:', error);
+    res.status(500).send('Internal Server Error');
+    return;
+  }
+});
+
+// Route to handle GET requests at the root URL
+app.get('/', (req, res) => {
+  // Display json data to the web UI.
+  res.send(transactionSummary);
+  console.log("localhost:3000 has been opened")
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
