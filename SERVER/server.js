c5423ab61d7d3c09efb4a2fa15e3a4902a96a7bf
Reizora <152237158+Reizora1@users.noreply.github.com>
2024-02-06 20:45:42 +0800

    Update server.js

--- SERVER/server.js
+++ SERVER/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors');//commit
 const firebase = require('firebase');
 const app = express();
 
