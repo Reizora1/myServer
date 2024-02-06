24678481181a3e356b4493ce235dbac080713077
Reizora <152237158+Reizora1@users.noreply.github.com>
2024-02-06 19:57:20 +0800

    Update server.js
    moved the paymentPage endpoint into this server.

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const firebase = require('firebase');
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 const firebaseConfig = {
   apiKey: "AIzaSyDxeU-bMAf-O0HYhz6X8yhsNPpqe19ld_8",
@@ -28,7 +33,10 @@ let transactionDate;
 let transactionSummary = "No Data received.";
 let transactionSummary2 = "No Data received.";
 
-// JSON Data parser
+//middleware
+app.use(express.static(path.join(__dirname), { 'extensions': ['css'], 'index': false }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // ignore favicon.ico requests
@@ -94,6 +102,74 @@ app.get('/machine2', (req, res) => {
   console.log(`localhost:${port} has been opened`)
 });
 
+app.get('/paymentPage', (req, res) => {
+    const htmlPath = path.join(__dirname,'paymentPage.html');
+    //const cssPath = path.join(__dirname, 'mainPageStyles.css');
+    const jsPath = path.join(__dirname, 'createInvoice.js');
+    
+    if (fileExists(htmlPath) /*&& fileExists(cssPath)*/ && fileExists(jsPath)) {
+        //const cssContent = fs.readFileSync(cssPath, 'utf8');
+        const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+        const jsContent = fs.readFileSync(jsPath, 'utf8');
+        res.send(`
+            ${htmlContent}
+        `);
+    } else {
+        res.status(404).send('404 Not Found');
+    }
+});
+
+app.get('/paymentPage2', (req, res) => {
+    const htmlPath = path.join(__dirname,'paymentPage2.html');
+    //const cssPath = path.join(__dirname, 'mainPageStyles.css');
+    const jsPath = path.join(__dirname, 'createInvoice2.js');
+    
+    if (fileExists(htmlPath) /*&& fileExists(cssPath)*/ && fileExists(jsPath)) {
+        //const cssContent = fs.readFileSync(cssPath, 'utf8');
+        const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+        const jsContent = fs.readFileSync(jsPath, 'utf8');
+        res.send(`
+            ${htmlContent}
+        `);
+    } else {
+        res.status(404).send('404 Not Found');
+    }
+});
+
+app.get('/paymentPage3', (req, res) => {
+    const htmlPath = path.join(__dirname,'paymentPage3.html');
+    //const cssPath = path.join(__dirname, 'mainPageStyles.css');
+    const jsPath = path.join(__dirname, 'createInvoice3.js');
+    
+    if (fileExists(htmlPath) /*&& fileExists(cssPath)*/ && fileExists(jsPath)) {
+        //const cssContent = fs.readFileSync(cssPath, 'utf8');
+        const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+        const jsContent = fs.readFileSync(jsPath, 'utf8');
+        res.send(`
+            ${htmlContent}
+        `);
+    } else {
+        res.status(404).send('404 Not Found');
+    }
+});
+
+app.get('/paymentPage4', (req, res) => {
+    const htmlPath = path.join(__dirname,'paymentPage4.html');
+    //const cssPath = path.join(__dirname, 'mainPageStyles.css');
+    const jsPath = path.join(__dirname, 'createInvoice4.js');
+    
+    if (fileExists(htmlPath) /*&& fileExists(cssPath)*/ && fileExists(jsPath)) {
+        //const cssContent = fs.readFileSync(cssPath, 'utf8');
+        const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+        const jsContent = fs.readFileSync(jsPath, 'utf8');
+        res.send(`
+            ${htmlContent}
+        `);
+    } else {
+        res.status(404).send('404 Not Found');
+    }
+});
+
 // writeData function
 function writeData() {
   let dataRef = null;
@@ -157,7 +233,15 @@ function writeData() {
       break;
   }
 }
-const port = 3000;
+
+function fileExists(filePath) {
+    try {
+        return fs.statSync(filePath).isFile();
+    } catch (err) {
+        return false;
+    }
+}
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
